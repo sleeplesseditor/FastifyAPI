79a2366ae7b00c26b73916a18e464e10ca0f0048
sleeplesseditor <josh@bryson.me.uk>
2018-12-14 01:14:04 +0000

    MLab connection established

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const fastify = require('fastify')({
     logger: true
-})
+});
+const mongoose = require('mongoose');
+const db = require('../config/keys').mongoURI;
   
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
-})
+});
+
+mongoose.connect(db)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err))
   
 const start = async () => {
     try {
